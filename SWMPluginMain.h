0bada34dc31c3a53709d498f26d8492a95af69fa
Kentzo <kulakov.ilya@gmail.com>
2010-02-05 01:02:30 +0600

    [RENAME] SWMPlugin -> SWMPluginMain

---
+++ SWMPluginMain.h
@@ -0,0 +1,18 @@
+//
+//  SWMPlugin.h
+//  SafariWindowManager
+//
+//  Created by Илья Кулаков on 02.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SWMPluginMain : NSObject {
+}
++ (SWMPluginMain*)sharedPlugin;
+
+- (void)safariWindowWillClose:(NSNotification *)notification;
+
+@end
