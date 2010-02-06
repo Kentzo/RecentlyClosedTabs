f0e4a3769126b9d9f8ed96e04f4266909ccf4290
Kentzo <kulakov.ilya@gmail.com>
2010-02-07 02:11:21 +0600

    [ADD] methods from dump of safari headers

--- DumpedSafariHeaders.h
+++ DumpedSafariHeaders.h
@@ -9,13 +9,26 @@
 
 @interface BrowserWindow
 - (id)windowController;
+- (NSRect)frame;
+- (NSInteger)level;
+- (id)currentTabViewItem;
 @end
 
 @interface BrowserWindowController
 - (id)orderedTabs;
+- (id)orderedTabViewItems;
+- (void)closeTab:(id)arg1;
+- (id)createTab;
 @end
 
 @interface BrowserWebView
 - (id)currentTitle;
 - (id)currentURL;
+- (id)document;
+- (void)goToURL:(id)arg1;
+- (id)undoManagerForWebView:(id)arg1;
+@end
+
+@interface BrowserTabViewItem
+- (id)webView;
 @end
