adffa24e86deea2dddfc0d7b3f59ddbd72c0d8fb
Kentzo <kulakov.ilya@gmail.com>
2010-02-07 01:54:43 +0600

    [MV] Notifications from all files to SWMNotifications

---
+++ SWMNotifications.h
@@ -0,0 +1,10 @@
+//
+//  SWMNotifications.h
+//  SafariWindowManager
+//
+//  Created by Илья Кулаков on 07.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+extern NSString* const SWMSafariTabWillCloseNotification;
+extern NSString* const SWMSafariTabIsOpenedNotification;
---
+++ SWMNotifications.m
@@ -0,0 +1,12 @@
+//
+//  SWMNotifications.m
+//  SafariWindowManager
+//
+//  Created by Илья Кулаков on 07.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SWMNotifications.h"
+
+NSString* const SWMSafariTabWillCloseNotification = @"SWMSafariTabWillCloseNotification";
+NSString* const SWMSafariTabIsOpenedNotification = @"SWMSafariTabIsOpenedNotification";
