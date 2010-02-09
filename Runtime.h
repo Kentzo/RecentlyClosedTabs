a9537c88a220c587abfc250dc2e06bb249b89eb0
Kentzo <kulakov.ilya@gmail.com>
2010-02-10 00:22:39 +0600

    [ADD] class_addMethodsFromClass function

---
+++ Runtime.h
@@ -0,0 +1,13 @@
+/*
+ *  Runtime.h
+ *  SafariWindowManager
+ *
+ *  Created by Илья Кулаков on 09.02.10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import <objc/objc-class.h>
+
+
+void class_addMethodsFromClass(Class dest, Class source);
---
+++ Runtime.m
@@ -0,0 +1,25 @@
+/*
+ *  Runtime.c
+ *  SafariWindowManager
+ *
+ *  Created by Илья Кулаков on 09.02.10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Runtime.h"
+
+void class_addMethodsFromClass(Class dest, Class source) {
+	// Get method list of source class
+	unsigned int list_size;
+	Method* methodList = class_copyMethodList(source, &list_size);
+	
+	// Add methods from the list to dest class
+	for(; list_size > 0; --list_size) {
+		Method meth = methodList[list_size - 1];
+		SEL sel = method_getName(meth);
+		const char* encod = method_getTypeEncoding(meth);
+		IMP imp = class_getMethodImplementation(source, sel);
+		class_addMethod(dest, sel, imp, encod);
+	}
+}
