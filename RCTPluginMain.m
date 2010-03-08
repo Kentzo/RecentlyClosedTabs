a2c90f4010084215750205241cac8f75cbc81a2c
Ilya Kulakov <kulakov.ilya@gmail.com>
2010-03-08 21:24:40 +0600

    [CHG] enable SpiceRack installer script in installer packproj [ADD] print some log messages during loading

--- RCTPluginMain.m
+++ RCTPluginMain.m
@@ -16,12 +16,13 @@ static RCTPluginMain* g_sharedPluginLoader = nil;
 @implementation RCTPluginMain
 
 + (void)load {
+    NSLog(@"RCT will start loading");
+    
 	NSError* error = nil;
     RCTToolbarButtonController* toolbarButtonController = [RCTToolbarButtonController new];
 
 	if (![CustomToolbarButtonExtension enableExtensionWithDelegate:toolbarButtonController error:&error]) {
 		NSLog(@"CustomToolbarButtonExtension was not loaded: %@", error);
-        return;
 	}
 	else {
 		NSLog(@"CustomToolbarButtonExtension was loaded");
@@ -29,13 +30,21 @@ static RCTPluginMain* g_sharedPluginLoader = nil;
 	
 	if (![DetectClosingTabExtension enableExtensionWithDelegate:toolbarButtonController error:&error]) {
 		NSLog(@"DetectClosingTabExtension was not loaded: %@", error);
-        return;
 	}
 	else {
 		NSLog(@"DetectClosingTabExtension was loaded");
 	}
+    // If error is occured will disable extensions and return
+    if (error != nil) {
+        [CustomToolbarButtonExtension disableExtension];
+        [DetectClosingTabExtension disableExtension];
+        return;
+    }
+    
     // Enable Sparkle
     [self pluginUpdater];
+    
+    NSLog(@"RCT did end loading");
 }
 
 + (SUUpdater*)pluginUpdater {
